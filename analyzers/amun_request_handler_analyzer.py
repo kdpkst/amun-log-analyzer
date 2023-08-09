fd3c5569c2657f984019f75854d866c88cf61cfb
kdpkst <jammiechen0@gmail.com>
2023-08-09 15:20:35 +0800

    develop the initial structure for main_analyzer.py

--- analyzers/amun_request_handler_analyzer.py
+++ analyzers/amun_request_handler_analyzer.py
@@ -5,21 +5,22 @@ import os
 
 class AmunRequestHandlerAnalyzer:
 
-    def data_processing(self, filename):
+    def data_report(self, filename, output_dir):
         parser = log_parser.AmunLogParser(filename)
         log_data = parser.amun_request_handler_log_parser()
 
-        # change the dictionary to sorted list
+        # sort the dictionary (the output is a list)
         IP_table = sorted(log_data['IP'].items(), key=lambda x: -x[1])
         port_table = sorted(log_data['port_scanned'].items(), key=lambda x : -x[1])
         stages_table = sorted(log_data['stages'].items(), key=lambda x: -x[1])
 
-        output_dir = 'output'
+        # improve it future (add subdire named AmunRequestHandler)
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
         
+        output_dir += '/'
         basename = os.path.basename(filename)
-        report_filename = 'output/statistics_' + basename 
+        report_filename = output_dir + basename.replace('.', '_') + '.stat' 
         with open(report_filename, 'w') as f:
             f.write("attacker IP        number\n")
             for item in IP_table:
@@ -34,8 +35,22 @@ class AmunRequestHandlerAnalyzer:
                 f.write(f"{item[0].ljust(20)}{item[1]}\n")
           
                 
-    def data_visulization(self):
+    def data_visulization(self, filename, output_dir='./output'):
+        parser = log_parser.AmunLogParser(filename)
+        log_data = parser.amun_request_handler_log_parser()
+
+        # sort the dictionary (the output is a list)
+        IP_table = sorted(log_data['IP'].items(), key=lambda x: -x[1])
+        port_table = sorted(log_data['port_scanned'].items(), key=lambda x : -x[1])
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+            
         return None
+
+    
+
+
 
 
 
--- main_analyzer.py
+++ main_analyzer.py
@@ -1,9 +1,46 @@
 import analyzers.amun_request_handler_analyzer as arha
+import argparse
+import os
+
 
 def main():
-    analyzer = arha.AmunRequestHandlerAnalyzer()
-    analyzer.data_processing('./amun_request_handler.log.2023-07-23')
-    
+    args = argument_parser()    
+    filename = args.filename
+    output_dir = args.output
+    logs_dir = args.recursion
+
+    if filename is None and logs_dir is None:
+        print("please specify arguments\nuse -h or --help to read help message")
+    else:
+        if filename is not None and logs_dir is not None:
+            print("wrong usage")
+        elif filename is not None:
+            argument_f(filename, output_dir)
+        elif logs_dir is not None:
+            argument_o(logs_dir, output_dir)
+ 
+
+def argument_parser():
+    parser = argparse.ArgumentParser(description="Amun Log Analyzer", usage="%(prog)s [-h] -f -o")
+    parser.add_argument("-f", "--filename", required=False, help="Path to the single log file")
+    parser.add_argument("-o", "--output", required=False, default='./output', help="Path to the output directory")
+    parser.add_argument("-r", "--recursion", required=False, help="Path to the directory storing multiple log files")
+
+    return parser.parse_args()
+
+
+def argument_f(filename, output_dir):
+    # whether filepath correct/exits?? improve it future
+    basename = os.path.basename(filename)
+    if basename.startswith('amun_request_handler'):
+        analyzer = arha.AmunRequestHandlerAnalyzer()
+        analyzer.data_report(filename, output_dir)
+
+
+# develop this part future
+def argument_o(logs_dir, output_dir):
+    pass
+
 
 if __name__== "__main__" :
     main()
